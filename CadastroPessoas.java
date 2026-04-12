99ba06817325dee499d0b47d7c401a2d88419811
Ian-pedro-passos <143625918+Ian-pedro-passos@users.noreply.github.com>
2026-04-12 10:46:13 -0300

    Update CadastroPessoas.java

--- CadastroPessoas.java
+++ CadastroPessoas.java
@@ -1,4 +1,23 @@
- if (altura == 0) break;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double altura;
+        double maior = 0;
+        double menor = Double.MAX_VALUE;
+        int homens = 0;
+        int mulheres = 0;
+        int totalPessoas = 0; // 👈 NOVO CONTADOR
+        String genero;
+
+        while (true) {
+            System.out.print("Digite a altura (0 para sair): ");
+            altura = sc.nextDouble();
+
+            if (altura == 0) break;
 
             if (altura > maior) maior = altura;
             if (altura < menor) menor = altura;
@@ -6,11 +25,17 @@
             System.out.print("Digite o gênero (m/f): ");
             genero = sc.next();
 
-            if (genero.equalsIgnoreCase("m")) homens++;
-            else if (genero.equalsIgnoreCase("f")) mulheres++;
+            if (genero.equalsIgnoreCase("m")) {
+                homens++;
+            } else if (genero.equalsIgnoreCase("f")) {
+                mulheres++;
+            }
+
+            totalPessoas++; // 👈 CONTA CADA PESSOA
         }
 
         System.out.println("\nRESULTADOS:");
+        System.out.println("Total de pessoas: " + totalPessoas); // 👈 NOVO
         System.out.println("Maior altura: " + maior);
         System.out.println("Menor altura: " + menor);
         System.out.println("Homens: " + homens);
